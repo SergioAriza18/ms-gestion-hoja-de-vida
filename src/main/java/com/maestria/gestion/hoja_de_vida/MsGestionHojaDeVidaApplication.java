bb60d2b2ac64b8e25e63980517b10dfb9da2ab7e
Sergio Ariza <sariza@unicauca.edu.co>
2026-02-04 20:26:38 -0500

    chore: inicializar proyecto Spring Boot del microservicio

---
+++ src/main/java/com/maestria/gestion/hoja_de_vida/MsGestionHojaDeVidaApplication.java
@@ -0,0 +1,13 @@
+package com.maestria.gestion.hoja_de_vida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionHojaDeVidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionHojaDeVidaApplication.class, args);
+	}
+
+}
